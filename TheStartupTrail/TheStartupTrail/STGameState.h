803710b21ba19b6d36e3797782c5eb56acb067e1
Philippe 'Peep' Chaintreuil <gitgeneral_peep@parallaxshift.com>
2012-05-05 19:33:52 -0400

    Started something to track game state.

---
+++ TheStartupTrail/TheStartupTrail/STGameState.h
@@ -0,0 +1,80 @@
+//
+//  STGameState.h
+//  TheStartupTrail
+//
+//  Created by Philippe Chaintreuil on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class STEmployee;
+
+struct MonthlyNumbers
+{
+	unsigned turnNum;
+	
+	int64_t cashInBank;
+	unsigned numCustomers;
+	unsigned customersWon;
+	unsigned customersLost;
+	unsigned numFeatures;
+	
+	int64_t cashFlow;
+	int64_t revenue;
+	int64_t expenses;
+	int64_t price;
+	
+	unsigned marketResearch;
+	unsigned sales;
+	unsigned features_work;
+	unsigned maint_work;
+	unsigned design_work;
+	unsigned ux_research;
+	
+	unsigned user_stories;
+	unsigned design_stories;
+	unsigned design;
+	unsigned complexity;
+	unsigned maint;
+};
+
+
+@interface STGameState : NSObject
+{
+	NSMutableArray *_employees;
+	struct MonthlyNumbers *_numbers;
+	
+	unsigned featuresPerDev;
+	unsigned featuresPerUserStoryPerDev;
+	unsigned maintPerDev;
+	
+	unsigned customersPerBiz;
+	unsigned customersPerFeature;
+	unsigned customersPerBizPerFeature;
+	unsigned userStoriesPerBiz;
+	
+	unsigned usabilityPerDesigner;
+	unsigned usabilityPerDesignStoryPerDev;
+	unsigned designStoriesPerDesigner;
+	
+	unsigned costPerEmployee;
+	unsigned pricePerFeature;
+	
+	unsigned customersLostDueToComplexity;
+	float maintRate;
+	unsigned customersLostDueToMaint;
+}
+
+@property (nonatomic, retain, readonly) NSMutableArray *employees;
+@property (nonatomic, assign) unsigned turnNumber;
+
++ (STGameState*) sharedGameState;
+
+- (void) generateTeam;
+
+- (void) nextTurn;
+
+- (void) updateNumbers;
+
+@end
---
+++ TheStartupTrail/TheStartupTrail/STGameState.m
@@ -0,0 +1,160 @@
+//
+//  STGameState.m
+//  TheStartupTrail
+//
+//  Created by Philippe Chaintreuil on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "STGameState.h"
+#import "STEmployee.h"
+
+STGameState *f_game_state = nil;
+
+@implementation STGameState
+
+@synthesize turnNumber;
+@synthesize employees = _employees;
+
+
+
++ (STGameState*) sharedGameState
+{
+	if (!f_game_state)
+	{
+		f_game_state = [[STGameState alloc] init];
+		assert(f_game_state);
+		
+		f_game_state->featuresPerDev = 1;
+		f_game_state->featuresPerUserStoryPerDev = 2;
+		f_game_state->maintPerDev = 10;
+		
+		f_game_state->customersPerBiz = 50;
+		f_game_state->customersPerFeature = 50;
+		f_game_state->customersPerBizPerFeature = 50;
+		f_game_state->userStoriesPerBiz = 5;
+		
+		f_game_state->usabilityPerDesigner = 1;
+		f_game_state->usabilityPerDesignStoryPerDev = 2;
+		f_game_state->userStoriesPerBiz = 1;
+		
+		f_game_state->costPerEmployee = 8000;
+		f_game_state->pricePerFeature = 5;
+		f_game_state->customersLostDueToComplexity = 50;
+		f_game_state->maintRate = 0.001f;
+		f_game_state->customersLostDueToMaint = 50;
+		
+		f_game_state->_employees = [[NSMutableArray alloc] initWithCapacity:8];
+		f_game_state->_numbers = malloc(sizeof(f_game_state->_numbers[0]));
+		assert(f_game_state->_numbers);
+		
+		struct MonthlyNumbers *mn = &f_game_state->_numbers[0];
+		mn->turnNum = 0;
+		mn->cashFlow = 100000;
+		mn->numCustomers = 0;
+		mn->customersWon = 0;
+		mn->customersLost = 0;
+		mn->numFeatures = 0;
+		mn->cashFlow = 0;
+		mn->revenue = 0;
+		mn->expenses = 0;
+		mn->price = 0;
+		mn->marketResearch = 0;
+		mn->sales = 0;
+		mn->features_work = 0;
+		mn->maint_work = 0;
+		mn->design_work = 0;
+		mn->ux_research = 0;
+		mn->user_stories = 0;
+		mn->design_stories = 0;
+		mn->design = 0;
+		mn->complexity = 0;
+		mn->maint = 0;
+	}
+	
+	return f_game_state;
+}
+
+- (void) generateTeam
+{
+	
+}
+
+- (void) nextTurn
+{
+	unsigned i = self.turnNumber;
+	self.turnNumber = i + 1;
+	self->_numbers = realloc(
+		self->_numbers, 
+		sizeof(self->_numbers[0]) * self.turnNumber);
+	assert(self->_numbers);
+	
+	memcpy(
+		&self->_numbers[i],
+		&self->_numbers[i-1],
+		sizeof(self->_numbers[0]) );
+}
+
+- (void) updateNumbers
+{
+	unsigned const i = self.turnNumber-1;
+	struct MonthlyNumbers *last_mn = &self->_numbers[i-1];
+	struct MonthlyNumbers *mn = &self->_numbers[i];
+	
+	mn->customersWon  = last_mn->sales * self->customersPerBiz;
+	mn->customersWon += last_mn->numFeatures * self->customersPerFeature;
+	mn->customersWon += last_mn->sales * last_mn->numFeatures * self->customersPerBizPerFeature;
+	
+	mn->customersLost  = last_mn->complexity * self->customersLostDueToComplexity;
+	mn->customersLost += last_mn->maint * self->customersLostDueToMaint;
+	
+	mn->numCustomers = last_mn->numCustomers - mn->customersLost +mn->customersWon;
+	
+	mn->user_stories  = last_mn->user_stories;
+	mn->user_stories += last_mn->marketResearch * self->userStoriesPerBiz;
+	unsigned storiesConverted = last_mn->features_work * self->featuresPerUserStoryPerDev;
+	storiesConverted = MIN(storiesConverted, last_mn->user_stories);
+	mn->user_stories -= storiesConverted;
+	
+	mn->numFeatures  = last_mn->numFeatures;
+	mn->numFeatures += last_mn->features_work * self->featuresPerDev;
+	mn->numFeatures += storiesConverted;
+
+	
+	mn->design_stories  = last_mn->design_stories;
+	mn->design_stories += last_mn->ux_research * self->designStoriesPerDesigner;
+	// :NOTE: Should remove converted tasks.
+	
+	mn->design  = last_mn->design;
+	mn->design += last_mn->design_work * self->usabilityPerDesigner;
+	unsigned t = last_mn->design_work * self->usabilityPerDesignStoryPerDev;
+	// :BUG: This doesn't look right.
+	mn->design += MIN(t, mn->design);
+	
+	mn->complexity = mn->numFeatures - mn->design;
+	mn->complexity = (unsigned)powf(mn->complexity, 1.2f);
+	
+	mn->maint = (unsigned)roundf(last_mn->numCustomers * self->maintRate);
+	
+	mn->marketResearch = 0;
+	mn->sales = 0;
+	mn->features_work = 0;
+	mn->maint_work = 0;
+	mn->design_work = 0;
+	mn->ux_research = 0;
+	
+	t = self.turnNumber + 1;
+	mn->price = mn->numFeatures - t;
+	mn->price = MAX(mn->price, 0);
+	mn->price *= self->pricePerFeature;
+	
+	mn->expenses = self.employees.count * self->costPerEmployee;
+	
+	mn->revenue = mn->price * mn->numCustomers;
+	
+	mn->cashFlow = mn->revenue - mn->expenses;
+	
+	mn->cashInBank = last_mn->cashInBank + mn->cashFlow;
+}
+
+@end
