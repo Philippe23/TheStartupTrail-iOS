67a2a5e7e33a32453393593e291c2211376ff1ef
Philippe 'Peep' Chaintreuil <gitgeneral_peep@parallaxshift.com>
2012-05-06 13:35:46 -0400

    Rename to remove repetition.

---
+++ TheStartupTrail/TheStartupTrail/STSponsorHackViewController.h
@@ -0,0 +1,16 @@
+//
+//  STSponsorHackViewControllerViewController.h
+//  TheStartupTrail
+//
+//  Created by Philippe Chaintreuil on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "STViewController.h"
+
+@interface STSponsorHackViewController : UIViewController
+
+- (IBAction) popOffNav:(id)sender;
+
+@end
